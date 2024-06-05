93f8fa853d421415ad4f8c8dd96647d12cd44127
Alexis-Prz21 <mp21030@ues.edu.sv>
2024-06-05 10:34:22 -0600

    Creación de vistas de registro y subida de imagen

---
+++ app/src/main/java/com/example/proyectoinnovacionpdm2024_gt2_grupo1/RegistroActivity.kt
@@ -0,0 +1,73 @@
+package com.example.proyectoinnovacionpdm2024_gt2_grupo1
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+
+class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var registrarEmail: EditText
+    private lateinit var registrarContra: EditText
+    private lateinit var registrarUs: Button
+    private lateinit var iniciarSesion: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+        registrarEmail = findViewById(R.id.regitrarEmailET)
+        registrarContra = findViewById(R.id.regitrarContraET)
+        registrarUs = findViewById(R.id.registrarUsBtn)
+        iniciarSesion = findViewById(R.id.tvIniciarSesion)
+
+        // Configurando las acciones del Button y del TextView
+        setup()
+    }
+
+    private fun setup() {
+        title = "Registro de Usuario"
+
+        registrarUs.setOnClickListener(){
+            if (registrarEmail.text.isNotEmpty() && registrarContra.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                        registrarEmail.text.toString(), registrarContra.text.toString()).addOnCompleteListener{
+                            if (it.isSuccessful) {
+                                mostrarPrincipal(it.result?.user?.email?: "")
+                            } else {
+                                val mensaje: String = "El usuario ya se encuentra registrado en el sistema"
+                                mostrarAlerta(mensaje)
+                            }
+                        }
+            }
+        }
+
+        iniciarSesion.setOnClickListener {
+            val intent = Intent(this,AuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun mostrarAlerta(mensaje:String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage(mensaje)
+        builder.setPositiveButton("Aceptar",null)
+        val dialogo: AlertDialog = builder.create()
+        dialogo.show()
+    }
+
+    private fun mostrarPrincipal(email: String) {
+        val principalIntent: Intent = Intent(this, PrincipalActivity::class.java).apply {
+            putExtra("email", email)
+        }
+        startActivity(principalIntent)
+    }
+}
---
+++ app/src/main/java/com/example/proyectoinnovacionpdm2024_gt2_grupo1/SubirImgActivity.kt
@@ -0,0 +1,17 @@
+package com.example.proyectoinnovacionpdm2024_gt2_grupo1
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.NonNull
+
+class SubirImgActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subir_img)
+
+    }
+
+}
