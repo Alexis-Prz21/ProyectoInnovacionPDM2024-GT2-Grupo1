3cddc0fbd6098463e56993d60437aa87615b9d4a
Alexis-Prz21 <mp21030@ues.edu.sv>
2024-06-05 10:41:07 -0600

    Creación de Floating Action Buttom

--- app/src/main/java/com/example/proyectoinnovacionpdm2024_gt2_grupo1/PrincipalActivity.kt
+++ app/src/main/java/com/example/proyectoinnovacionpdm2024_gt2_grupo1/PrincipalActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 
 class PrincipalActivity : AppCompatActivity() {
 
     private lateinit var emailText: TextView
     private lateinit var cerrarSesion: Button
+    private lateinit var agregarFab: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +20,7 @@ class PrincipalActivity : AppCompatActivity() {
 
         emailText = findViewById(R.id.tvEmail)
         cerrarSesion = findViewById(R.id.cerrarSesionBtn)
+        agregarFab = findViewById(R.id.fab)
 
         val bundle = intent.extras
         val email:String? = bundle?.getString("email")
@@ -33,6 +36,11 @@ class PrincipalActivity : AppCompatActivity() {
             FirebaseAuth.getInstance().signOut()
             // Volviendo a la pantalla anterior
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        agregarFab.setOnClickListener {
+            val intent = Intent(this,SubirImgActivity::class.java)
+            startActivity(intent)
         }
     }
 }
