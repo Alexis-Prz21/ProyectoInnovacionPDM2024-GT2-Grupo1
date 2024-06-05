3ef0b5511c985a9e6a07865331e95b327b5c2378
Alexis-Prz21 <mp21030@ues.edu.sv>
2024-06-05 10:39:45 -0600

    Separación de la pantalla de login de la pantalla de registros

--- app/src/main/java/com/example/proyectoinnovacionpdm2024_gt2_grupo1/AuthActivity.kt
+++ app/src/main/java/com/example/proyectoinnovacionpdm2024_gt2_grupo1/AuthActivity.kt
@@ -1,16 +1,18 @@
 package com.example.proyectoinnovacionpdm2024_gt2_grupo1
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity : AppCompatActivity() {
 
-    private lateinit var registrar: Button
+    private lateinit var registrar: TextView
     private lateinit var acceder: Button
     private lateinit var email: EditText
     private lateinit var contra: EditText
@@ -19,7 +21,7 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        registrar = findViewById(R.id.registrarBtn)
+        registrar = findViewById(R.id.tvRegistrar)
         acceder = findViewById(R.id.accederBtn)
         email = findViewById(R.id.emailtET)
         contra = findViewById(R.id.contraET)
@@ -29,22 +31,7 @@ class AuthActivity : AppCompatActivity() {
     }
 
     private fun setup() {
-        title = "Autenticación"
-
-        registrar.setOnClickListener(){
-            if (email.text.isNotEmpty() && contra.text.isNotEmpty()) {
-                FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(
-                    email.text.toString(), contra.text.toString()).addOnCompleteListener{
-
-                    if (it.isSuccessful) {
-                        mostrarPrincipal(it.result?.user?.email?: "")
-                    } else {
-                        mostrarAlerta()
-                    }
-                }
-            }
-        }
+        title = "Login"
 
         acceder.setOnClickListener(){
             if (email.text.isNotEmpty() && contra.text.isNotEmpty()) {
@@ -55,17 +42,23 @@ class AuthActivity : AppCompatActivity() {
                         if (it.isSuccessful) {
                             mostrarPrincipal(it.result?.user?.email?: "")
                         } else {
-                            mostrarAlerta()
+                            val mensaje: String = "El usuario no se encuentra registrado en el sistema"
+                            mostrarAlerta(mensaje)
                         }
                     }
             }
         }
+
+        registrar.setOnClickListener {
+            val intent = Intent(this,RegistroActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    private fun mostrarAlerta() {
+    private fun mostrarAlerta(mensaje:String) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
-        builder.setMessage("Se ha producido un error al autenticar al usuario")
+        builder.setMessage(mensaje)
         builder.setPositiveButton("Aceptar",null)
         val dialogo: AlertDialog = builder.create()
         dialogo.show()
